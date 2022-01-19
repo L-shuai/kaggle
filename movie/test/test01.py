872b3ec54b4393099d67118d69e0d46a6414435a
L-shuai <1472174772@qq.com>
2022-01-19 03:08:05 +0000

    modify

---
+++ movie/test/test01.py
@@ -0,0 +1,26 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:LiShuai
+@Blog(个人博客地址): https://www.cnblogs.com/L-shuai/
+ 
+@File:test01.py
+@Time:2022/1/17 16:40
+ 
+"""
+
+
+def test1():
+    word2idx = {}
+    word2idx['a'] = 1
+    print(len(word2idx))
+    word2idx[99]=3
+    print(len(word2idx))
+
+
+def test2():
+    arr = [1,2,3,5]
+    print(arr[1:-1])
+
+if __name__ == '__main__':
+    test2()
